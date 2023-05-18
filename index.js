c76ced60b9fc8f618534b0a9deab00f97bdf1685
tirahman105 <tirahman105@gmail.com>
2023-05-18 23:15:55 +0600

    toy data create , read done

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 
-// toyAdmin
-// yZeaFjOSxvxfph97
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kx4dtgt.mongodb.net/?retryWrites=true&w=majority`;
@@ -30,11 +28,33 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const toysCollection = client.db('toysDB').collection('toys');
+
+
+
+    app.get('/toys', async(req, res) => {
+      console.log(req.query.email);
+      let query = {};
+      if(req.query?.email){
+        query = {email: req.query.email}
+      }
+      const result = await toysCollection.find(query).toArray();
+      res.send(result)
+    })
+
+
+    app.get('/toys', async(req, res) =>{
+      const cursor =toysCollection.find()
+      const result = await cursor.toArray();
+      res.send(result);
+    })
 
 
     app.post('/toys', async(req, res) => {
       const toy =req.body;
       console.log('new toy', toy);
+      const result = await toysCollection.insertOne(toy);
+      res.send(result);
   })
 
     // Send a ping to confirm a successful connection
