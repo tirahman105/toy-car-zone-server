4ffba85b68c9979a24ef378b5f74d436a3ede25f
tirahman105 <tirahman105@gmail.com>
2023-05-19 15:02:32 +0600

    details api created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000
@@ -70,6 +70,15 @@ async function run() {
       const result = await toysCollection.insertOne(toy);
       res.send(result);
   })
+
+
+  app.get('/toys/details/:id', async(req, res) => {
+    const id = req.params.id;
+    const query = {_id: new ObjectId (id)}
+    const result = await toysCollection.findOne(query);
+    res.send(result);
+  })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
